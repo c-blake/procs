e4276db831d7a0f12fbc09750ab6fd06ec1f5062
Charles Blake <charlechaud@gmail.com>
2019-10-12 09:32:08 -0400

    Exit status semantics closer to my original intent.

--- procs.nim
+++ procs.nim
@@ -1375,8 +1375,8 @@ proc act(actions: seq[PdAct], pid: Pid, delim: string, sigs: seq[cint],
   for a in actions:
     case a
     of acEcho : stdout.write pid, delim
-    of acKill : nErr += (kill(pid, sigs[0]) != -1).int
-    of acNice : nErr += (nice(pid, nice.cint) != -1).int
+    of acKill : nErr += (kill(pid, sigs[0]) == -1).int
+    of acNice : nErr += (nice(pid, nice.cint) == -1).int
     of acWait1: discard
     of acWaitA: discard
     of acCount: cnt.inc
@@ -1476,7 +1476,7 @@ proc find*(pids="", full=false, parent: seq[Pid] = @[], pgroup: seq[Pid] = @[],
     var dt = delay - (getTime() - t0)
     if dt < delay: dt.nanosleep
     for i, sig in sigs[1..^1]:
-      for pid in pList: result += (kill(pid, sig) != 0).int
+      for pid in pList: result += (kill(pid, sig) == -1).int
       if i < sigs.len - 2: delay.nanosleep
   if pList.len > 0:                           #wait for condition if requested
     if   acWait1 in actions: discard waitAny(pList, delay)
