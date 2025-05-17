5331341f1aacfff545b41c1556b0de9d27bc1ff3
Charles Blake <charlechaud@gmail.com>
2025-05-17 02:19:58 -0400

    Eh.. Go ahead and add some example script wrappers of `parc`.

---
+++ util/parc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+: "${j:=$(ncpu /2)}"
+: "${t:=/dev/shm}"; export t
+: "${prog:=s / r /stat r /cmdline R /exe r /io r /schedstat r /smaps_rollup}"
+export PARC_PATHS="sys/kernel/pid_max uptime" prog
+cd /proc; set [1-9]*    # Just to count procs to divide by j
+echo $* | xargs --process-slot-var=J -n "$((1+$#/j))" -P "$j" \
+ /bin/sh -c 'exec>>$t/p.$J;[ $J -eq 0 ]||unset PARC_PATHS;exec parc $prog $*' d0
+cat $t/p.* > $t/pJ
+rm -f $t/p.*
---
+++ util/parc.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh -f
+: "${j:=$(ncpu /2)}"    # j jobs parallel /proc files data collector
+t="/dev/shm"            # temp dir
+cd "/proc"              # proc-fs root
+pids=([1-9]*)           # All processes *right now*; Inherently racy
+n=${#pids}              # Number total
+m=$(( (n + j - 1)/j ))  # Batch size
+for ((i=1; i<=j; i++)); do # Fire $j batches
+    slice=(${(@)pids[$(( (i-1)*m + 1 )),$((i*m <= n ? i*m : n))]})
+# `procs display` style drives specific list needed here.  To show MyStyle needs
+# `PFA=/tmp/x pd -sMyStyle; cpio -tv</tmp/x|tail`, but NOTE unreadable entries
+# (eg. kthread /exe, otherUser /smaps_rollup) are dropped from the cpio archive.
+    [[ $i = 1 ]] && export PARC_PATHS="sys/kernel/pid_max uptime"
+    parc s /   r /stat      r /cmdline      R /exe \
+         r /io r /schedstat r /smaps_rollup $slice > $t/pfs.$$.$i &
+    unset PARC_PATHS
+done
+wait                    # Wait for last to finish
+cat $t/pfs.$$.* >$t/pfs # Merge results
+rm -f $t/pfs.$$.*       # Clean-up temporary files
