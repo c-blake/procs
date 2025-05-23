c08d50330aca04db4b856d9d8161f9a5b5e1149d
Charles Blake <charlechaud@gmail.com>
2025-05-23 05:41:12 -0400

    Change usage to reflect command-line syntax overhaul to be much simpler in common cases, but note that this is a BREAKING CHANGE.  See `Use` for the new syntax.
    Other notable changes:
    
     - Move to Nim strings for an `av` commandLineParams() to simplify work
       of the new addDirents().
    
     - Rip out all the debugging & signal handling code after Nim & gcc-14
       version incompatibility identified as culprit for racy zero buffers.
    
     - Move to a single global `st` buffer and IO `buf`.
    
     - No longer padding to 512 byte blocks (which requires annoying
       bookkeeping if output is a pipe anyway, and also does not seem
       to be needed by GNU cpio, bsdcpio, or paxcpio), we move to Pad
       as a single byte pad buffer and `pad0` as a pointer to it.
    
     - Add `odd` utility function to make up-rounding/padding simpler.
    
     - Use both odd & pad0 appropriately.
    
     - Move strlen decl to the one proc it's still used.
    
     - Introduce soProg (start-of-program) variable to pair with eoProg for
       the new CLI syntax.
    
     - Adapt / simplify perPidWork() in light of new CLI syntax.
       Specifically, drop unneeded `i` copy, re-use new global `st`, do not
       warn on non-numeric top-level dirs since that can only come from
       explicit user request anyway, use new `av` instead of `argv`, and
       make program loop dense over soProg..<eoProg.
    
     - Change parc_quiet to just q and use existence test so q= parc works.
    
     - Sleep a little less on fork or pipe failures, though if this matters
       your system is in a world of hurt.
    
     - `driveKids` mostly the same except using new `odd` and renaming bytes
       to `toDo` and ripping out debugging code.
    
     - addDirents() { run only if user doesn't provide a 3rd argv block }
       now does a directory scan and filter to add "1", "2" .. to `av`.
    
     - main() is really overhauled for the new CLI syntax documented in the
       new `Use`, but grew a $d interpretation as well.  Another new feature
       is that j=0 will use all CPUs while j=-3 will use "all but 3".  j=-1
       often is at least recommendable since the parent here works a lot.

--- util/parc.nim
+++ util/parc.nim
@@ -1,42 +1,27 @@
-const u = """/proc archiver like cpio -oHbin, but works on weird /proc files.
+const Use="""Programmed ARChiver; Like cpio -oHbin, but works on odd /proc files
 Usage:
-  cd /proc
-  j=4 PARC_PATHS='sys/kernel/pid_max uptime meminfo' parc s / r /stat R /exe \
-   r /cmdline r /io r /schedstat r /smaps_rollup [1-9]* >/dev/shm/$USER-pfs.cpio
-
-Here s* means stat, r* means read, R* means ReadLink, $j indicates parallelism,
-and $PARC_PATHS is split-on-space for global(non-perPID) paths done first.
-
-`PFA=x pd -sX; cpio -tv < x | less` shows global and per-PID needs of style X,
-but NOTE unreadable entries (e.g. kthread /exe, otherUser /io) are dropped from
-cpio archives, which are then treated as empty files by `pd`."""
+  d=DIR(/proc) j=JOBS(1) parco GLOBALS -- PROGRAM [-- ROOTS]
+where DIR is the directory to run out of (must be cd-able), JOBS refers to
+process parallelism like make -j, GLOBALS are $d-relative paths to archive,
+PROGRAM is a series of archival steps to make against ROOTS.  If ROOTS are
+omitted, every top-level direntry of $d matching [1-9]* is used.  Program
+steps are LETTER/entry where LETTER codes are s:stat r:read R:ReadLink and
+these actions are run with each ROOT as a prefix to \"/entry\".  E.g.:
+  j=8 parc sys/kernel/pid_max uptime meminfo -- s/ r/stat r/io R/exe \\
+           r/cmdline r/schedstat r/smaps_rollup >/dev/shm/$LOGNAME-pfs.cpio
+snapshots what `procs display` needs for most formats/sorts/merges. To see
+needs for your specific case of style X, `PFA=y pd -sX; cpio -tv < y|less`,
+but NOTE parc drops unreadable entries(eg. kthread /exe, otherUser /io) from
+written cpio archives, which are then treated as empty files by `pd`."""
 
 when not declared stderr: import std/syncio
 import std/[posix, os, strutils], cligen/[posixUt, osUt]
-proc cstrlen(s: pointer): int {.importc: "strlen", header: "string.h".}
-let argc {.importc: "cmdCount".}: cint        # On POSIX, not a lib; importc
-let argv {.importc: "cmdLine".}: cstringArray #..is both simpler & faster.
+var av = commandLineParams()
+
 proc add(s: var string, t: cstring, nT: int) =  # Sort of like a C `memcat`
   let n0 = s.len
   s.setLen n0 + nT
-  if s.len > 0:                     # Both s and t could be ""
-    copyMem s[n0].addr, t, nT
-
-proc hdl(sigNo: cint) {.noconv.} =    # Temporary; Later will likely be ignored
-  if sigNo == SIGCHLD: return
-  discard write(2, "parc: DELIVERED SIGNAL: ".cstring, 24)
-  var ch = chr(ord('@') + sigNo.int)  # Notably, SIGCHLD has a default ignore
-  discard write(2, ch.addr, 1)        # disposition but this mvs it to print,
-  discard write(2, "\n".cstring, 1)   # maybe generating EINTR/etc.
-
-when defined busyWait:
-  const busyWait {.intdefine.} = 200
-  import std/hashes
-  proc wk(buf: cstring; len, j: int) =
-    var h = hash(j + 123456789)
-    for round in 1..busyWait:         #nim c -d:busyWait=50 or 300 to edit delay
-      for c in 0..<len: h = (h shl 17) or (h shr 47) xor buf[c].int
-    if h == 1234567890123456789: quit "UNLIKELY COLLISION", 83
+  if s.len > 0: copyMem s[n0].addr, t, nT       # Both s and t could be ""
 
 type Rec* {.packed.} = object       # Saved data header; cpio -oHbin compatible
   magic, dev, ino, mode, uid, gid, nlink, rdev: uint16 # magic=0o070707
@@ -45,9 +30,12 @@ type Rec* {.packed.} = object       # Saved data header; cpio -oHbin compatible
   datLen: array[2, uint16]              #26B Hdr; Support readFile stat readlink
 
 var rec = Rec(magic: 0o070707)          # Heavily re-used CPIO record header
-var pad0: array[512, char]              # Just a big pad buffer of all \0
+var st: Stat
+var buf: string
+var Pad = '\0'; var pad0 = Pad.addr     # Just a 1 byte pad buffer for \0
 let (o, e) = (stdout, stderr)           # Short aliases
 proc clear(rec: var Rec) = zeroMem(rec.addr, rec.sizeof); rec.magic = 0o070707
+proc odd[T: SomeInteger](n: T): int = int(n.int mod 2 != 0)
 
 proc writeRecHdr(path: cstring; pLen, datLen: int) =
   let path = if path.isNil: nil else: cast[pointer](path)
@@ -55,13 +43,9 @@ proc writeRecHdr(path: cstring; pLen, datLen: int) =
   rec.nmLen     = uint16(pLen + 1)      # Include NUL terminator
   rec.datLen[0] = uint16(datLen shr 16)
   rec.datLen[1] = uint16(datLen and 0xFFFF)
-  let bytes = rec.nmLen.int + int(rec.nmLen mod 2 != 0) +
-              datLen        + int(datLen mod 2 != 0)
-  if bytes < 2:                         # This should simply not be possible
-    e.write "parc: SHORT WRITE: nameLen=",rec.nmLen," dataLen=",datLen,"\n"
   discard o.uriteBuffer(rec.addr, rec.sizeof)
   discard o.uriteBuffer(path    , pLen + 1)
-  if pLen mod 2 == 0: discard o.uriteBuffer(pad0.addr, 1)
+  if pLen mod 2 == 0: discard o.uriteBuffer(pad0, 1)
 
 proc fromStat(rec: var Rec; st: ptr Stat) =
   rec.dev      = uint16(st[].st_dev)    # Dev should neither change nor matter..
@@ -75,9 +59,9 @@ proc fromStat(rec: var Rec; st: ptr Stat) =
   rec.mtime[1] = uint16(st[].st_mtime.int and 0xFFFF)
 
 proc stat(a1: cstring, a2: var Stat): cint = # Starts "program" for PID subDirs
+  proc strlen(s: pointer): int {.header: "string.h".}
   discard posix.stat(a1, a2) # rec.clear unneeded: stat sets EVERY field anyway
-  rec.fromStat a2.addr
-  writeRecHdr a1, a1.cstrlen, 0; flushFile o
+  rec.fromStat a2.addr; writeRecHdr a1, a1.strlen, 0; flushFile o
 
 proc readFile(path: string, buf: var string, st: ptr Stat=nil, perRead=4096) =
   posixUt.readFile path, buf, st, perRead # Does an fstat ONLY IF `st` not Nil
@@ -86,7 +70,7 @@ proc readFile(path: string, buf: var string, st: ptr Stat=nil, perRead=4096) =
     else: rec.mode = 0o100444; rec.nlink = 1 # Regular file r--r--r--; Inherit..
     writeRecHdr path.cstring, path.len, buf.len #..rest from needed last stat.
     discard o.uriteBuffer(buf.cstring, buf.len)
-    if buf.len mod 2 == 1: discard o.uriteBuffer(pad0.addr, 1)
+    if buf.len mod 2 == 1: discard o.uriteBuffer(pad0, 1)
     flushFile o
 
 proc readlink(path: string, err=e): string = # Must follow `s` "command"
@@ -95,44 +79,39 @@ proc readlink(path: string, err=e): string = # Must follow `s` "command"
     rec.mode = 0o120777; rec.nlink = 1  # Inherit rest from needed last stat.
     writeRecHdr path.cstring, path.len, result.len + 1
     discard o.uriteBuffer(result.cstring, result.len + 1)
-    if result.len mod 2 == 0: discard o.uriteBuffer(pad0.addr, 1)
+    if result.len mod 2 == 0: discard o.uriteBuffer(pad0, 1)
     flushFile o
 
-var jobs = 1; var i, eoProg: int              # Globals to all parallel work
+var jobs=1; var soProg=1; var eoProg, i: int  # Globals to all parallel work
 var thisUid: Uid                              # Const during execution, EXCEPT i
 proc perPidWork(remainder: int) =
-  var i = i     # fork already gives kid a private copy of `i`,but this is cheap
-  var st: Stat
-  var buf, path: string
-  while i < argc:
+  template `+!`(p: cstring, i: int): cstring = cast[cstring](cast[int](p) +% i)
+  var path: string
+  while i < av.len:
     if i mod jobs != remainder: inc i; continue
-    if argv[i][0] notin {'1'..'9'}:
-      e.write "parc warning: \"", $argv[i], "\" cannot be a PID\n"
-    path.setLen 0; let nI = cstrlen(argv[i]); path.add argv[i], nI
-    for j in countup(1, eoProg - 1, 2):
-      path.setLen nI; let nJ = cstrlen(argv[j + 1]); path.add argv[j + 1], nJ
-      if   argv[j][0] == 's': discard stat(path.cstring, st)
-      elif argv[j][0] == 'r':
+    path = av[i]; let nI = path.len
+    for j in soProg..<eoProg:           # Form "ROOT/entry" in `path`
+      path.setLen nI; path.add av[j].cstring +! 1, av[j].len - 1
+      if   av[j][0] == 's': discard stat(path.cstring, st)
+      elif av[j][0] == 'r':
         if path == "/smaps_rollup":
           if thisUid == 0 or thisUid == st.st_uid: readFile path, buf
         else: readFile path, buf
-      elif argv[j][0] == 'R': discard readlink(path, nil)
+      elif av[j][0] == 'R': discard readlink(path, nil)
     inc i
 
 proc driveKids() =
-  var sa = Sigaction(sa_handler: hdl) # SIG_IGN if this turns out to problem
-  for sig in cint(1)..31: discard sigaction(sig, sa)
-  let quiet = getEnv("parc_quiet", "").len >= 3
+  let quiet = existsEnv("q")
   var pipes = newSeq[array[0..1, cint]](jobs)
   var fds   = newSeq[TPollfd](jobs)
   for j in 0..<jobs:            # Re-try rather than exit on failures since..
     while pipe(pipes[j]) < 0:   #..often one queries /proc DUE TO overloads.
       if not quiet: e.write "parc: pipe(): errno: ",errno,"\n"
-      discard usleep(20_000)    # Microsec; So, 20 ms|50/sec
+      discard usleep(10_000)    # Microsec; So, 20 ms|50/sec
     var kid: Pid
     while (kid = fork(); kid == -1):
       if not quiet: e.write "parc: fork(): errno: ",errno,"\n"
-      discard usleep(20_000)    # Microsec; So, 20 ms|50/sec
+      discard usleep(10_000)    # Microsec; So, 20 ms|50/sec
     if kid == 0:                # In Kid
       discard pipes[j][0].close
       if dup2(pipes[j][1], 1) < 0: quit "parc: dup2 failure - bailing", 4
@@ -142,59 +121,62 @@ proc driveKids() =
       discard pipes[j][1].close
       fds[j] = TPollfd(fd: pipes[j][0], events: POLLIN)
   var buf = newSeq[char](4096)
-  var nLive = jobs; var k = i
+  var nLive = jobs
   while nLive > 0:
     if poll(fds[0].addr, jobs.Tnfds, -1) <= 0:
       if errno == EINTR: continue
       quit "parc: poll(): errno: " & $errno, 5
     for j in 0..<jobs:
       template cp1 =    # Write already read header `rec` & then cp varLen data
-        if nR != rec.sizeof: e.write "parc: SHORT PIPE READ: ",nR," BYTES\n"
-        when defined busyWait:wk cast[cstring](fds[0].addr),jobs*fds[0].sizeof,j
-        let dLen = (rec.datLen[0].int shl 16) or rec.datLen[1].int
-        let bytes = rec.nmLen.int + int(rec.nmLen mod 2 != 0) +
-                    dLen + int(dLen mod 2 != 0)         # Calculate size
-        if bytes < 2:   # Do not forge onward with impossible/bad data
-          e.write "parc: SHORT RECORD: nameLen=",rec.nmLen," dataLen=",dLen,
-                  " forOutputArg=",k,"/",argc,"\n"; quit 73
         discard o.uriteBuffer(rec.addr, rec.sizeof)     # Send header to stdout
-        buf.setLen bytes                                # Read all, blocking..
-        while (let nR=read(fds[j].fd, buf[0].addr, bytes); nR<0): #..as needed.
+        let dLen = (rec.datLen[0].int shl 16) or rec.datLen[1].int
+        let toDo = rec.nmLen.int + rec.nmLen.odd + dLen + dLen.odd # Calc size
+        buf.setLen toDo                                 # Read all, blocking..
+        while (let nR=read(fds[j].fd, buf[0].addr, toDo); nR<0): #..as needed.
           discard usleep(500) #; e.write "parc: had to wait\n"
-        discard o.uriteBuffer(buf[0].addr, bytes)       # Send body to stdout
-        inc k
+        discard o.uriteBuffer(buf[0].addr, toDo)        # Send body to stdout
       if fds[j].fd != -1 and fds[j].revents != 0:
         if (fds[j].revents and POLLIN) != 0:                # Data is ready
           if (let nR = read(fds[j].fd, rec.addr, rec.sizeof); nR > 0): cp1
-          else:
-            dec nLive; if close(fds[j].fd)==0: fds[j].fd = -1 else: quit 6
+          else: (dec nLive; if close(fds[j].fd)==0: fds[j].fd = -1 else: quit 6)
         if (fds[j].revents and POLLHUP) != 0:               # Kid is done
           while (var nR = read(fds[j].fd, rec.addr, rec.sizeof); nR > 0): cp1
           dec nLive; if close(fds[j].fd)==0: fds[j].fd = -1 else: quit 7
 
+proc addDirents() =     # readdir("."), appending $dir/[1-9]* to av[], ac
+  let fd = open(".", 0o200000) # O_DIRECTORY
+  var dts: seq[int8]
+  let dents = getDents(fd, st, dts.addr, avgLen=10)
+  for j, dent in dents:
+    if dts[j] == DT_DIR and dent[0] in {'1'..'9'}: av.add dent
+  discard fd.close
+
 proc main() =
-  if argc < 2 or argv[1][0] in {'\0', '-'}: quit u
-  var buf: string; var st: Stat
-  if chdir("/proc") != 0: quit "cannot cd /proc"
+  if av.len < 1 or av[0].len < 1 or av[0][0] == '-': quit Use, 1
+  let dir = getEnv("d", "/proc");
+  if chdir(dir.cstring) != 0: quit "cannot cd " & dir, 2
   thisUid = getuid()
-  for f in getEnv("PARC_PATHS", "sys/kernel/pid_max uptime meminfo").split:
-    if f.len > 0: readFile f, buf, st.addr
-  i = 1; while i < argc:
-    if argv[i][0] in {'1'..'9'}:
-      break
-    if i mod 2 == 1 and argv[i][0] notin {'s', 'r', 'R'}:
-      e.write "Bad command ",$argv[i]," (not s*|r*|R*)\n\n"; quit u, 1
-    if i mod 2 == 0 and argv[i][0] != '/':
-      e.write "expecting /dirEntry as in /proc/PID/dirEntry\n\n"; quit u, 2
+  while i < av.len:
+    if av[i] == "--": inc i; soProg = i; break
+    if av[i].len > 0: readFile av[i], buf, st.addr
     inc i
-  if argv[1][0] != 's':
-    e.write "'program' doesn't start w/stat; /smaps_rollup trim may fail\n"
-  eoProg = i
-  if eoProg mod 2 != 1: quit "unpaired 'program' args", 3
-  jobs = getEnv("j", "1").parseInt
-  if jobs == 1: perPidWork 0
-  else: driveKids()
-  rec.clear; rec.nlink = 1                  # At least GNU cpio does this;Cannot
-  writeRecHdr cstring("TRAILER!!!"), 10, 0  #..pad w/o seekable(!PIPE) o assump.
+  if soProg >= av.len: soProg = av.len - 1
+  if av[soProg][0] != 's': e.write "parc: PROGRAM doesn't start w/\"s\"tat\n"
+  while i < av.len:
+    if av[i] == "--": break
+    if av[i].len < 2 or av[i][1] != '/' or av[i][0] notin {'s','r','R'}:
+      quit "bad command \""&av[i]&"\" (not [srR]/XX)", 4
+    inc i
+  eoProg = max(i, soProg)
+  if eoProg<=soProg: e.write "No PROGRAM; Start(",soProg,") >= End(",eoProg,")\n"
+  else:
+    if i < av.len: inc i                # skip "--" for next for loop
+    if i == av.len: addDirents()
+    if (jobs = getEnv("j", "1").parseInt; jobs <= 0):
+      jobs += sysconf(SC_NPROCESSORS_ONLN)
+      if jobs < 0: jobs = 1
+    if jobs == 1: perPidWork 0
+    else: driveKids()
+  rec.clear; rec.nlink = 1; writeRecHdr cstring("TRAILER!!!"), 10, 0
 
 main()
